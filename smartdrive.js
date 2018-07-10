79a6e8d49709cb242c4afa1d58d9c5a030223cf7
Bennie <brownbe21@gmail.com>
2018-07-10 19:12:22 -0400

    update

--- smartdrive.js
+++ smartdrive.js
@@ -2,18 +2,14 @@
 const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-/** Set your phone numbers here. They must be 11-digit numbers that start with
-  * the "1" for phones within the United States.
-  * 
-  * Use commas after each number except for the last number.
-  */
+
+  
 const PHONE_NUMBER_ARRAY = [
    
-  "12293951253" 
+  "12223333333" 
    
 ]; 
 
-/** Set your 3 messages here. */
 const SINGLE_CLICK_MESSAGE = "ONE CLICK OF THE BUTTON";
 const DOUBLE_CLICK_MESSAGE = "Let's do lunch this week.";
 const LONG_CLICK_MESSAGE = "I need help. Please come over now!";
